606ecb9715b9fed4d7cbe245177d582c06c12573
Ahlijati Nuraminah <ahlijati.nuraminah@gmail.com>
2022-03-15 10:20:46 +0700

    Meeting 8: Move player with animation

--- src/scenes/CoronaBusterScene.js
+++ src/scenes/CoronaBusterScene.js
@@ -11,6 +11,9 @@ export default class CoronaBusterScene extends Phaser.Scene
         this.nav_left = false
         this.nav_right = false
         this.shoot = false
+        this.player = undefined
+        this.speed = 100
+        this.cursor = undefined
     }
     
     preload()
@@ -20,6 +23,7 @@ export default class CoronaBusterScene extends Phaser.Scene
         this.load.image('left-btn', 'images/left-btn.png')
         this.load.image('right-btn', 'images/right-btn.png')
         this.load.image('shoot-btn', 'images/shoot-btn.png')
+        this.load.spritesheet('player', 'images/ship.png', {frameWidth:66, frameHeight:66})
     }
     
     create()
@@ -35,6 +39,9 @@ export default class CoronaBusterScene extends Phaser.Scene
         })
         Phaser.Actions.RandomRectangle(this.clouds.getChildren(), this.physics.world.bounds);
         this.createButton()
+        this.player = this.createPlayer()
+
+        this.cursors = this.input.keyboard.createCursorKeys()
 
     }
     
@@ -51,19 +58,21 @@ export default class CoronaBusterScene extends Phaser.Scene
             // @ts-ignore
             child.y = child.displayHeight * -1
             }
-            })
+        })
+
+        this.movePlayer(this.player)
     }
 
     createButton()
     {
         this.input.addPointer(3)
         let shoot = this.add.image(320, 550, 'shoot-btn')
-        .setInteractive().setDepth(0.5).setAlpha(0.8)
+            .setInteractive().setDepth(0.5).setAlpha(0.8)
         let nav_left = this.add.image(50, 550, 'left-btn')
-        .setInteractive().setDepth(0.5).setAlpha(0.8)
+            .setInteractive().setDepth(0.5).setAlpha(0.8)
         let nav_right = this.add.image(nav_left.x + nav_left
-        .displayWidth + 20, 550, 'right-btn')
-        .setInteractive().setDepth(0.5).setAlpha(0.8)
+            .displayWidth + 20, 550, 'right-btn')
+            .setInteractive().setDepth(0.5).setAlpha(0.8)
 
         nav_left.on('pointerdown', () => { this.nav_left = true }, this)
         nav_left.on('pointerout', () => { this.nav_left = false}, this)
@@ -71,5 +80,45 @@ export default class CoronaBusterScene extends Phaser.Scene
         nav_right.on('pointerout', () => { this.nav_right = false}, this)
         shoot.on('pointerdown', () => { this.shoot = true }, this)
         shoot.on('pointerout', () => { this.shoot = false}, this)
+    }
+
+    createPlayer(){
+        const player = this.physics.add.sprite(200, 450, 'player')
+        player.setCollideWorldBounds(true)
+
+        this.anims.create({
+            key:'turn',
+            frames: [{key: 'player', frame:0 }]
+        })
+
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('player', {start: 1, end: 2}),
+            frameRate: 10
+        })
+
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('player', {start: 1, end: 2}),
+            frameRate: 10
+        })
+
+        return player
+    }
+
+    movePlayer(player){
+        if(this.cursors.left.isDown || this.nav_left){
+            this.player.setVelocityX(this.speed * -1)
+            this.player.anims.play('left', true)
+            this.player.setFlipX(false)
+        }else if(this.cursors.right.isDown || this.nav_right){
+            this.player.setVelocityX(this.speed)
+            this.player.anims.play('right', true)
+            this.player.setFlipX(true)
+        }else{
+            this.player.setVelocityX(0)
+            this.player.anims.play('turn')
+        }
+
     }
 }
